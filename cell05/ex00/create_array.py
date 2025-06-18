774dbc34a9ed5a3e5ddde373d5fab5321dbccf35
jiraphach <jirapach29cake@gmail.com>
2025-06-18 03:30:33 +0000

    00

---
+++ cell05/ex00/create_array.py
@@ -0,0 +1,10 @@
+#่ตั้งค่า array ที่เก็บเลขหลายตัว
+numbers = [2, 8, 9,48,8,22,-12, 2]
+
+#แสดง array ใน terminal
+print("Array ที่เก็บเลขหลายตัว:")
+print(numbers)
+
+# แสดงผลลัพธ์ตามที่กำหนด
+print("\nผลลัพธ์:")
+print(numbers)
